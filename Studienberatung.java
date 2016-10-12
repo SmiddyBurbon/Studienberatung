773e6aa0495c74fa098f9d51bc05487047e8cf96
desch296 <me@dennisschmidt.net>
2016-10-12 15:15:57 +0200

    added first question

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class Studienberatung {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Hast du Abitur?");
+		scanner.nextLine();
 	}
 }
