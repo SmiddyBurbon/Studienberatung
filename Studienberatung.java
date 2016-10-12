f4b697c1e3c131224390712c3f2deae1b5068b8e
desch296 <me@dennisschmidt.net>
2016-10-12 15:26:24 +0200

    added second question

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,8 +3,14 @@ import java.util.*;
 public class Studienberatung {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		String answer;
 
 		System.out.println("Hast du Abitur?");
-		scanner.nextLine();
+		answer = scanner.nextLine();
+
+		if(answer.equalsIgnoreCase("n")) {
+			System.out.println("Hast du Fachabitur?");
+			answer = scanner.nextLine();
+		}
 	}
 }
